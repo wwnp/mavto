f9c968b7e6996ab80a6aaa3f6ba5293c22ce5de8
wwnp <iamtodaysad@gmail.com>
2021-09-16 04:50:04 +0300

    add update fn

--- src/components/Slider.component.js
+++ src/components/Slider.component.js
@@ -12,19 +12,11 @@ export class Slider extends Component {
     this.track = this.$el.querySelector('#sliderTrack')
     this.prev = this.$el.querySelector('#arrow_prev')
     this.next = this.$el.querySelector('#arrow_next')
-
-    this.store['counter'] = this.counter
-    this.store['sl_height'] = this.$el.offsetHeight
-    this.store['sl_width'] = Math.round(this.store['sl_height'] * 1.777777777)
-    this.store['maxWidth'] = this.store['sl_width'] * this.slides.length
-
-    this.$el.style.width = this.store['sl_width'] + 'px'
-    this.track.style.width = (this.store['sl_width'] * this.slides.length) + 'px'
-
-    this.slides.forEach(element => {
-      element.children[0].style.height = this.store['sl_height'] + 'px'
-      element.children[0].style.width = this.store['sl_width'] + 'px'
-    });
+    
+    update.call(this)
+    window.onresize = ()=> {
+      update.call(this)
+    }
 
     this.prev.addEventListener('click', prevHandler.bind(this))
     this.next.addEventListener('click', nextHandler.bind(this))
@@ -47,4 +39,18 @@ function nextHandler() {
     this.store['counter'] = 0
   }
   this.track.style.transform = `translate3d(${this.store['counter']}px,0px,0px)`
+}
+function update(){
+  this.store['counter'] = this.counter
+  this.store['sl_height'] = this.$el.offsetHeight
+  this.store['sl_width'] = window.outerWidth
+  this.store['maxWidth'] = this.store['sl_width'] * this.slides.length
+
+  this.$el.style.width = this.store['sl_width'] + 'px'
+  this.track.style.width = (this.store['sl_width'] * this.slides.length) + 'px'
+
+  this.slides.forEach(element => {
+    element.children[0].style.height = this.store['sl_height'] + 'px'
+    element.children[0].style.width = this.store['sl_width'] + 'px'
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import { Slider } from "./components/Slider.component.js";
 
 const slider = new Slider('slider')
-
